d372c418e72ba36a608e140ce7deaff94a218c4f
hunternbird <hnbird115@gmail.com>
2025-01-17 14:50:27 -0700

    Program and DiceSimulator Class

---
+++ DiceSimulator.cs
@@ -0,0 +1,21 @@
+namespace RollDice;
+
+public class DiceSimulator
+{
+    public int[] SimulateRolls(int NumOfRolls)
+    {
+        int[] rollResults = new int[13];
+        
+        Random random = new Random();
+        
+        for (int i = 0; i < NumOfRolls; i++)
+        {
+            int die1 = random.Next(1, 7); // Roll the first die (1-6)
+            int die2 = random.Next(1, 7); // Roll the second die (1-6)
+            int sum = die1 + die2;        // Sum of the two dice
+            rollResults[sum]++;          // Increment the count for this sum
+        }
+        return rollResults;
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace RollDice;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("How many dice rolls do you want to simulate?");
+        int NumOfRolls = int.Parse(Console.ReadLine());
+
+
+        DiceSimulator simulator = new DiceSimulator();
+        
+        int[] rollResults = simulator.SimulateRolls(NumOfRolls);
+        
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {NumOfRolls}.\n");
+
+        for (int i = 2; i <= 12; i++) // Only display results for sums 2-12
+        {
+            double percentage = (rollResults[i] / (double)NumOfRolls) * 100;
+            int stars = (int)Math.Round(percentage); // Each star represents 1%
+            
+            Console.WriteLine($"{i}: {new string('*', stars)}");
+        }
+        
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+        
+    }
+}
